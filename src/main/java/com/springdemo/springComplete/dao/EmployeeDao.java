72714e09c00429c518e8a7bf8060d68c1ce532d1
Joe <joe@s2h.ca>
2021-06-23 13:05:38 +0530

    initial commit

---
+++ src/main/java/com/springdemo/springComplete/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.springdemo.springComplete.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import com.springdemo.springComplete.entity.Employee;
+
+public interface EmployeeDao extends JpaRepository<Employee, Integer> {
+
+	Optional<List<Employee>> findByName(String name);
+}
---
+++ src/main/java/com/springdemo/springComplete/resources/EmployeeController.java
@@ -0,0 +1,70 @@
+package com.springdemo.springComplete.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.springdemo.springComplete.entity.Employee;
+import com.springdemo.springComplete.service.EmployeeService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@GetMapping
+	public List<Employee> getEmployees(){
+		return employeeService.getEmploees();
+	}
+	
+	@GetMapping("/{id}")
+	public Employee	 getEmployeeById(@PathVariable("id") int id){
+		return employeeService.getEmployeeById(id);
+	}
+	
+	
+	@GetMapping("name/{name}")
+	public List<Employee> searchByNmae(@PathVariable("name") String name, @PathVariable int id){
+		return employeeService.getEmployeeByName(name);
+	}
+	
+	
+	@GetMapping("search")
+	public List<Employee> searchBySome(@RequestParam(defaultValue = "ramesh") String name){
+		return employeeService.getEmployeeByName(name);
+	}
+	
+
+	@PostMapping("/post")
+	public Employee insertEmployee(@RequestBody @Valid Employee employee){
+		return employeeService.insertEmployee(employee);
+	}
+
+	@PutMapping("/put")
+	public Employee updateEmployee(@RequestBody @Valid Employee employee){
+		return employeeService.updateEmployee(employee);
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public String deleteEmployee(@PathVariable("id") int employeeId){
+		 employeeService.deleteEmployee(employeeId);
+		 return employeeId+" successfully deleted";
+	}
+	
+}
